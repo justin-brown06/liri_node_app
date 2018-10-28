c5cc5954067971fc7df441d384dd376711acf636
Justin.B.Brown <brownee06@gmail.com>
2018-10-27 18:08:44 -0700

    Update Homework.  Mainly bandsintown API

--- liri.js
+++ liri.js
@@ -1,4 +1,80 @@
+//
 require("dotenv").config();
+
+const inquirer = require("inquirer");
+const keys = require("./keys");
+const Spotify = require("spotify-web-api-node");
+const request = require("request");
 
 var spotify = new Spotify(keys.spotify);
 
+var artist;
+
+function concertThis() {
+    inquirer
+    .prompt([{
+        type: "input",
+        message: "Please search an artist for venues:",
+        name: "userArtist"
+    }]).then( function(concertRes){
+        artist = concertRes.userArtist;
+        let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+        request(queryUrl, function(error, response, body) {
+
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+          
+              // Parse the body of the site and recover just the imdbRating
+              // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+              console.log("Upcoming venue: " + response.body);
+            }
+          });
+
+    });
+};
+
+let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+// https://rest.bandsintown.com/artists/katastro/events?app_id=codingbootcamp
+
+// console.log(queryUrl);
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Choose an option to search: ",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+            name: "userOption"
+        }
+    ]).then(function (inqRes) {
+        if (inqRes.userOption == "concert-this") {
+            concertThis();
+        }
+    });
+
+// switch(action) {
+//     case "concert-this":
+
+//         request(queryUrl, function(error, response, body) {
+
+//             // If the request is successful
+//             if (!error && response.statusCode === 200) {
+
+//               // Parse the body of the site and recover just the imdbRating
+//               // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//               console.log(JSON.parse(body).Year);
+//             }
+//           });//Will need to create a .then(for loop) to access multiple venues from the return array/object
+
+//     break;
+//     case "spotify-this-song":
+
+//     break;
+//     case "movie-this":
+
+//     break;
+//     case "do-what-it-says":
+
+//     break;
+// };
---
+++ psuedo.md
@@ -0,0 +1,9 @@
+# Todo List:
+
+Use inquirer to make a list of the possible commands in the app.
+
+**If MVP is finished** Possibly look into using inquirer to give a list of options upon the answer of a list of questions.
+
+### Checklist:
+Confirm that the spotify API, bands-in-town API, and omdb API are working correctly.
+
