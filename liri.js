573d2bd083a8e8c07f7b9e1e06364af6fd2ba85a
Justin.B.Brown <brownee06@gmail.com>
2018-10-29 15:43:18 -0700

    Updated Spotify, OMDB, and BandsInTown

--- liri.js
+++ liri.js
@@ -1,9 +1,8 @@
-//
 require("dotenv").config();
 
 const inquirer = require("inquirer");
 const keys = require("./keys");
-const Spotify = require("spotify-web-api-node");
+const Spotify = require("node-spotify-api");
 const request = require("request");
 const moment = require("moment");
 
@@ -20,7 +19,7 @@ function concertThis() {
         }]).then(function (concertRes) {
             artist = concertRes.userArtist;
             let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-            // https://rest.bandsintown.com/artists/katastro/events?app_id=codingbootcamp
+            //! Example: https://rest.bandsintown.com/artists/katastro/events?app_id=codingbootcamp
 
             request(queryUrl, function (error, response, body) {
 
@@ -33,7 +32,7 @@ function concertThis() {
                         console.log("--------------------");
                         console.log("Venue Name: " + result[i].venue.name);
                         console.log("Venue Location: " + result[i].venue.city + ", " + result[i].venue.region);
-                        console.log("Venue Date: " + moment().format(JSON.stringify(result[i].datetime), moment.ISO_8601, "MM-DD-YYYY"));
+                        console.log("Venue Date: " + moment(JSON.stringify(result[i].datetime), ["YYYY", moment.ISO_8601]).format("MM-DD-YYYY"));
                         console.log("--------------------");
                     };
                 };
@@ -48,19 +47,17 @@ function spotifyThisSong() {
             message: "Please search a song to spotify:",
             name: "userSong"
         }]).then(function (songRes) {
-            console.log("You chose a song!" + JSON.stringify(songRes));
-            // let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-            // request(queryUrl, function(error, response, body) {
-
-            // // If the request is successful
-            // if (!error && response.statusCode === 200) {
-
-            //   // Parse the body of the site and recover just the imdbRating
-            //   // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            //   console.log("Upcoming venue: " + body);
-            // }
-            //   });
+            spotify
+                .search({ type: 'track', query: songRes.userSong, limit: 3})
+                .then(function (response) {
+                    console.log(response.tracks.items[0].name);
+                    console.log(response.tracks.items[0].artists[0].name);
+                    console.log(response.tracks.items[0].album.name);
+                    console.log(response.tracks.items[0].external_urls.spotify);
+                })
+                .catch(function (err) {
+                    console.log(err)
+                });
 
         });
 };
@@ -75,18 +72,14 @@ function movieThis() {
             var movieName = resMovie.userMovie
             // Then run a request to the OMDB API with the movie specified
             var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-            //http://www.omdbapi.com/?t=goodfellas&y=&plot=short&apikey=trilogy
-
-            // This line is just to help us debug against the actual URL.
-            console.log(queryUrl);
+            //! Example: http://www.omdbapi.com/?t=goodfellas&y=&plot=short&apikey=trilogy
 
             request(queryUrl, function (error, response, body) {
 
                 // If the request is successful
                 if (!error && response.statusCode === 200) {
 
-                    // Parse the body of the site and recover just the imdbRating
-                    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+                    // Display the information of the movie.
                     console.log("Movie Title: " + JSON.parse(body).Title);
                     console.log("Release Year: " + JSON.parse(body).Year);
                     console.log("IMDB Rating: " + JSON.parse(body).Ratings[1].Value);
@@ -95,7 +88,7 @@ function movieThis() {
                     console.log("Language of Movie: " + JSON.parse(body).Language);
                     console.log("Plot of the movie: " + JSON.parse(body).Plot);
                     console.log("Actors in the: " + JSON.parse(body).Actors);
-                }
+                };
             });
 
         });
@@ -109,23 +102,9 @@ function doWhatItSays() {
             name: "userSong"
         }]).then(function (doItRes) {
             console.log("You chose to do something!" + JSON.stringify(doItRes));
-            // let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-            // request(queryUrl, function(error, response, body) {
-
-            // // If the request is successful
-            // if (!error && response.statusCode === 200) {
-
-            //   // Parse the body of the site and recover just the imdbRating
-            //   // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            //   console.log("Upcoming venue: " + body);
-            // }
-            //   });
 
         });
 };
-
-// console.log(queryUrl);
 
 inquirer
     .prompt([
@@ -136,45 +115,19 @@ inquirer
             name: "userOption"
         }
     ]).then(function (inqRes) {
-        if (inqRes.userOption == "concert-this") {
+        if (inqRes.userOption === "concert-this") {
             concertThis();
         }
-        if (inqRes.userOption == "spotify-this-song") {
+        if (inqRes.userOption === "spotify-this-song") {
             console.log("You chose to spotify this song.")
             spotifyThisSong();
         }
-        if (inqRes.userOption == "movie-this") {
+        if (inqRes.userOption === "movie-this") {
             console.log("You chose to search this movie.")
             movieThis();
         }
-        if (inqRes.userOption == "do-what-it-says") {
+        if (inqRes.userOption === "do-what-it-says") {
             console.log("You chose to do your own thing.")
             doWhatItSays();
         }
     });
-
-// switch(action) {
-//     case "concert-this":
-
-//         request(queryUrl, function(error, response, body) {
-
-//             // If the request is successful
-//             if (!error && response.statusCode === 200) {
-
-//               // Parse the body of the site and recover just the imdbRating
-//               // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//               console.log(JSON.parse(body).Year);
-//             }
-//           });//Will need to create a .then(for loop) to access multiple venues from the return array/object
-
-//     break;
-//     case "spotify-this-song":
-
-//     break;
-//     case "movie-this":
-
-//     break;
-//     case "do-what-it-says":
-
-//     break;
-// };
